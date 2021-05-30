6fd6f1b7904d0edbd7db1a825d6d12d7fc5da538
Aditya Hajare <aditya43@gmail.com>
2021-05-30 06:41:16 +0530

    Range over channel

--- 02-channels/02-range-over/main.go
+++ 02-channels/02-range-over/main.go
@@ -5,11 +5,11 @@ import "fmt"
 func main() {
 	ch := make(chan int)
 	go func() {
+		defer close(ch) // Close channel
 		for i := 0; i < 6; i++ {
 			// send iterator over channel
 			ch <- i
 		}
-		close(ch) // Close channel
 	}()
 
 	// range over channel to recv values
